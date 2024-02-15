814bf3fb776bbd6edae0473626cff72550859fae
saleh sheikh <salehsheikh786@gmail.com>
2024-02-16 03:06:08 +0500

    updated

--- src/components/Parent.jsx
+++ src/components/Parent.jsx
@@ -1,25 +1,89 @@
-import React, { useState } from 'react';
-import Form from './Form';
-import { Cards } from './Cards';
-
-const ParentComponent = () => {
+import React, { useState, useEffect } from "react";
+import Form from "./Form";
+import { Cards } from "./Cards";
+import ExpenseForm from "./ExpenseForm";
+const Parent = () => {
   const [budget, setBudget] = useState(0);
   const [inputBudget, setInputBudget] = useState(0);
+  const [expense, setExpense] = useState(0);
+  const [inputExpense, setInputExpense] = useState(0);
+  const [remainingBudget, setRemainingBudget] = useState(0);
+  const [incomeName, setIncomeName] = useState("");
+  const [incomeAmount, setIncomeAmount] = useState("");
+  // Update remaining budget whenever budget or expense changes
+  useEffect(() => {
+    const remaining = budget - expense;
+    setRemainingBudget(remaining);
+  }, [budget, expense]);
 
   const handleBudgetChange = (e) => {
     setInputBudget(e.target.value);
   };
 
-  const handleSetBudget = () => {
+  const handleSetBudget = (e) => {
     setBudget(inputBudget);
   };
 
+  //for updating
+  const handleNameChange = (e) => {
+    setIncomeName(e.target.value);
+  };
+
+  const handleAmountChange = (e) => {
+    setIncomeAmount(e.target.value);
+  };
+  // Handler for adding income
+  const handleAddIncome = () => {
+    // Convert incomeAmount and current budget to numbers
+    const currentBudget = parseFloat(budget);
+    const income = parseFloat(incomeAmount);
+
+    // Add income to the budget
+    setBudget(currentBudget + income);
+
+    // Clear input fields
+    setIncomeName("");
+    setIncomeAmount("");
+  };
+
+  //handler for adding  expense
+  const handleExpenseChange = (e) => {
+    setInputExpense(e.target.value);
+  };
+
+  const handleSetExpense = (e) => {
+    setExpense(inputExpense);
+  };
+
+  const handleResetBudget=()=>{
+    setBudget("");
+    setIncomeAmount("");
+  }
+
   return (
     <div>
-      <Form handleBudgetChange={handleBudgetChange} handleSetBudget={handleSetBudget} />
-      <Cards budget={budget} />
+      <Cards
+        budget={budget}
+        expense={expense}
+        remainingBudget={remainingBudget}
+        incomeAmount={incomeAmount}
+      />
+      <Form
+        handleBudgetChange={handleBudgetChange}
+        handleSetBudget={handleSetBudget}
+        handleAddIncome={handleAddIncome}
+        handleAmountChange={handleAmountChange}
+        handleNameChange={handleNameChange}
+        incomeAmount={incomeAmount}
+        incomeName={incomeName}
+        handleResetBudget={handleResetBudget}
+      />
+      <ExpenseForm
+        handleExpenseChange={handleExpenseChange}
+        handleSetExpense={handleSetExpense}
+      />
     </div>
   );
 };
 
-export default ParentComponent;
+export default Parent;
