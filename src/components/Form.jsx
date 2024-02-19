0cf7d58da0d66b588ccf4231287d66415046ba1a
saleh sheikh <salehsheikh786@gmail.com>
2024-02-20 01:58:23 +0500

    production ready app

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,13 +1,102 @@
-import React from 'react';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
+import React from "react";
+import { TextField, Box, Typography, useMediaQuery } from "@mui/material";
+import Button from "@mui/material/Button";
 
-const Form = ({ handleBudgetChange, handleSetBudget }) => {
+const Form = ({
+  handleBudgetChange,
+  handleSetBudget,
+  handleAmountChange,
+  handleAddIncome,
+  handleResetBudget,
+  handleNameChange,
+  incomeAmount,
+  incomeName,
+}) => {
+  // Use useMediaQuery hook to check screen size
+  
+  const isSmallScreen = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+
   return (
-    <div>
-      <TextField id="outlined-basic" label="Enter Budget" variant="outlined" onChange={handleBudgetChange} />
-      <Button variant="contained" onClick={handleSetBudget}>Set Budget</Button>
-    </div>
+    <Box
+      display="flex"
+      flexDirection="column"
+      alignItems="start"
+      mt={2}
+      ml={26}
+    >
+      <TextField
+        sx={{ width: isSmallScreen ? "100%" : "50%", marginBottom: "8px" }} // Adjusted width based on screen size
+        id="outlined-basic"
+        label="Enter Budget"
+        variant="outlined"
+        type="number"
+        onChange={handleBudgetChange}
+      />
+      <Button
+        variant="contained"
+        fontFamily={"cursive"}
+        onClick={handleSetBudget}
+        sx={{
+          width: isSmallScreen ? "100%" : "10%", // Adjusted width based on screen size
+          bgcolor: "#121935",
+          marginTop: "8px",
+        }}
+      >
+        Set Budget
+      </Button>
+
+      <Typography variant="h6" mt={2}>
+       Update budget
+      </Typography>
+      <TextField
+        sx={{ width: isSmallScreen ? "100%" : "50%", marginBottom: "25px" }} // Adjusted width based on screen size
+        id="income-name"
+        label="Income Name"
+        variant="outlined"
+        onChange={handleNameChange}
+      />
+      <TextField
+        sx={{
+          width: isSmallScreen ? "100%" : "50%",
+          marginBottom: "16px", // Increased margin-bottom
+        }}
+        id="income-amount"
+        label="Income Amount"
+        variant="outlined"
+        type="number"
+        onChange={handleAmountChange}
+      />
+      <Button
+        variant="contained"
+        fontFamily={"cursive"}
+        onClick={() => handleAddIncome(incomeName, incomeAmount)}
+        sx={{
+          width: isSmallScreen ? "100%" : "10%", // Adjusted width based on screen size
+          bgcolor: "#1976d2",
+          marginTop: "8px",
+          marginBottom: "20px",
+        }}
+      >
+        Add Income
+      </Button>
+      <Typography variant="h6" gutterBottom fontFamily={"cursive"}>
+        Reset Your Budget
+      </Typography>
+      <Typography gutterBottom fontFamily={"cursive"}>
+        Resets your budget back to 0
+      </Typography>
+      <Button
+        variant="contained"
+        onClick={handleResetBudget}
+        sx={{
+          width: isSmallScreen ? "100%" : "10%", // Adjusted width based on screen size
+          bgcolor: "#f44336",
+          marginTop: "12px",
+        }}
+      >
+        Reset Budget
+      </Button>
+    </Box>
   );
 };
 
